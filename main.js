24ea3a89ac425673540f7cb23c47b6634e7cdc22
Tânia <106488757+meganPopy@users.noreply.github.com>
2025-10-12 17:58:41 -0300

    a

--- main.js
+++ main.js
@@ -58,7 +58,6 @@ function geraSenha() {
     }
     campoSenha.value = senha;
     classificaSenha(alfabeto.length);
-
 }
 
 function classificaSenha(tamanhoAlfabeto){
@@ -73,7 +72,8 @@ function classificaSenha(tamanhoAlfabeto){
         forcaSenha.classList.add('fraca');
     }
     const valorEntropia = document.querySelector('.entropia');
-    valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
+    valorEntropia.textContent = "Um computador pode levar até " + 
+Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
 }
 
 
